b5705bd8eb967f523970cb009d886b74d3198553
SureshVJ <vjsuresh20021@yahoo.co.in>
2021-06-13 18:32:32 +0530

    My First STS commit(06:32PM,13-06-2021)

---
+++ STSGitHubProject1/src/main/java/com/example/gitdemo/StsGitHubProject1Application.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitHubProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitHubProject1Application.class, args);
+		System.out.println("I am in SpringBoot");
+	}
+
+}
