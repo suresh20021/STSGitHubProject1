7d67bafddc6b0db39cc91c40993069f1b61a97c2
SureshVJ <vjsuresh20021@yahoo.co.in>
2021-06-13 18:52:38 +0530

    added a class GitDemo

---
+++ STSGitHubProject1/src/main/java/com/example/gitdemo/mygitdemo/GitDemo.java
@@ -0,0 +1,9 @@
+package com.example.gitdemo.mygitdemo;
+
+public class GitDemo {
+	
+	public void myMethod() {
+		// TODO Auto-generated method stub
+		System.out.println("Just an demo for git");
+	}
+}
